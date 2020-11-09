83d03a0480d0329f925f3ed67a661d75a313c733
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-11-08 21:55:21 -0300

    feat: introduce initial view controller to search screen

---
+++ Rango/Rango/Modules/Search/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Rango
+//
+//  Created by Rhian Lopes da Costa on 08/11/20.
+//  Copyright © 2020 CWI. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
