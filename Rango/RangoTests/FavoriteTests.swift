9df6c406f06e1e91a9068ad6b1bc2f7d57bc43ce
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-11-30 20:19:15 -0300

    feat: introduce final unit tests to favorite screen

--- Rango/RangoTests/FavoriteTests.swift
+++ Rango/RangoTests/FavoriteTests.swift
@@ -40,7 +40,43 @@ class FavoriteTests: QuickSpec {
                     expect(result).to(beFalse())
                 }
             }
-            
+            context("when remove cards") {
+                it("should remove some card") {
+                    let recipe: Recipe = Recipe(id: 1, userId: 1, name: "", bio: "", dificulty: Dificult.easy, time: 1, rating: 1, image: "", ingredients: [""], steps: [""])
+                    
+                    favorite.favoriteRecipes = [recipe]
+                    
+                    favorite.remove(recipe: recipe)
+                    expect(favorite.favoriteRecipes).to(beEmpty())
+                }
+                it("should dont remove any card") {
+                    let recipe: Recipe = Recipe(id: 1, userId: 1, name: "", bio: "", dificulty: Dificult.easy, time: 1, rating: 1, image: "", ingredients: [""], steps: [""])
+                    
+                    favorite.favoriteRecipes = []
+                    
+                    favorite.remove(recipe: recipe)
+                    expect(favorite.favoriteRecipes).to(beEmpty())
+                }
+            }
+            context("when check favorite card") {
+                it("should be a favorite card") {
+                    let recipe: Recipe = Recipe(id: 1, userId: 1, name: "", bio: "", dificulty: Dificult.easy, time: 1, rating: 1, image: "", ingredients: [""], steps: [""])
+                    
+                    favorite.favoriteRecipes = [recipe]
+                    
+                    let result = favorite.checkFavoriteRecipe(recipe)
+                    expect(result).to(beTrue())
+                }
+                it("should dont be a favorite card") {
+                    let firstRecipe: Recipe = Recipe(id: 1, userId: 1, name: "", bio: "", dificulty: Dificult.easy, time: 1, rating: 1, image: "", ingredients: [""], steps: [""])
+                    let secondRecipe: Recipe = Recipe(id: 2, userId: 1, name: "", bio: "", dificulty: Dificult.easy, time: 1, rating: 1, image: "", ingredients: [""], steps: [""])
+                    
+                    favorite.favoriteRecipes = [firstRecipe]
+                    
+                    let result = favorite.checkFavoriteRecipe(secondRecipe)
+                    expect(result).to(beFalse())
+                }
+            }
         }
     }
 }
